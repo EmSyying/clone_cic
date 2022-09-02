3d962fa8ae6a4b32f47dc088e0cd4673d97a2764
Srun Tola <tola.srun@z1fsc.com>
2022-09-02 09:46:15 +0700

    update

--- lib/Utils/app_settings/models/slide_model.dart
+++ lib/Utils/app_settings/models/slide_model.dart
@@ -1,31 +1,45 @@
-class SlideModel {
-  SlideModel({
-    this.title,
-    this.description,
-    this.type,
-    this.status,
-    this.image,
-  });
+import '../../../modules/notification_modules/models/notification_data.dart';
 
+class SlideModel {
   String? title;
   String? description;
   String? type;
   String? status;
   String? image;
+  List<Button>? button;
 
-  factory SlideModel.fromJson(Map<String, dynamic> json) => SlideModel(
-        title: json["title"],
-        description: json["description"],
-        type: json["type"],
-        status: json["status"],
-        image: json["image"],
-      );
+  SlideModel(
+      {this.title,
+      this.description,
+      this.type,
+      this.status,
+      this.image,
+      this.button});
 
-  Map<String, dynamic> toJson() => {
-        "title": title,
-        "description": description,
-        "type": type,
-        "status": status,
-        "image": image,
-      };
+  SlideModel.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    description = json['description'];
+    type = json['type'];
+    status = json['status'];
+    image = json['image'];
+    if (json['button'] != null) {
+      button = <Button>[];
+      json['button'].forEach((v) {
+        button!.add(Button.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    data['description'] = description;
+    data['type'] = type;
+    data['status'] = status;
+    data['image'] = image;
+    if (button != null) {
+      data['button'] = button!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
 }
