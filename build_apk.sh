eb89d1d6315b443b1c51a50317e742814509d370
Srun Tola <tola.srun@z1fsc.com>
2022-10-21 20:44:25 +0700

    update flavor and build script for build apk

---
+++ build_apk.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# [allow permission to run script]
+# chmod u+rx build_apk.sh
+# [Run the script]
+# ./build_apk.sh [env-name]
+
+if [ "$1" = "dev" ];
+then
+    #clean folder build
+    flutter clean
+    # flutter pub get
+    flutter pub get
+    # start build apk for production
+    flutter build apk --flavor dev -t lib/main_dev.dart
+elif [ "$1" = "pre" ];
+then 
+    # clean folder build
+    flutter clean
+    # flutter pub get
+    flutter pub get
+    # start build apk for production
+    flutter build apk --flavor pre -t lib/main_pre.dart
+elif [ "$1" = "prod" ];
+then 
+    # clean folder build
+    flutter clean
+    # flutter pub get
+    flutter pub get
+    # start build apk for production
+    flutter build apk --flavor prod -t lib/main_prod.dart
+else   
+    echo "Please select a env"
+fi
+
---
+++ lib/main_pre.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+import 'Utils/function/get_sharepreference_data.dart';
+import 'Utils/function/notification_helper.dart';
+import 'Utils/helper/local_storage.dart';
+import 'core/flavor/flavor_configuration.dart';
+import 'main.dart';
+
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await LocalStorage.init();
+  // WidgetsBinding.instance.addPostFrameCallback((_) => initPlugin());
+  // setPathUrlStrategy();
+  // await NotificationHelper.initial();
+  // await Firebase.initializeApp();
+  // SystemChrome.setSystemUIOverlayStyle(
+  //   SystemUiOverlayStyle.light.copyWith(
+  //     systemNavigationBarIconBrightness: Brightness.dark,
+  //   ),
+  // );
+  // SystemChrome.setPreferredOrientations(
+  //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  // await GlobalConfiguration().loadFromAsset("app_settings");
+
+  // runApp(
+  //   MyApp(),
+  // );
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await LocalStorage.init();
+    await Firebase.initializeApp();
+    await GlobalConfiguration().loadFromAsset("app_settings");
+    setPathUrlStrategy();
+    await settingCon.fetchAppSetting();
+    await NotificationHelper.initial();
+    optionCon.fetchAllOptions();
+    await LocalData.getCurrentUser().then(
+      (value) {
+        if (value != null) {
+          isLogin.value = true;
+          isLogin.notifyListeners();
+        } else {
+          isLogin.value = false;
+          isLogin.notifyListeners();
+        }
+      },
+    );
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    FlavorConfig(
+        flavor: Flavor.PRE,
+        color: Colors.deepPurpleAccent,
+        values: FlavorValues(
+            baseUrl: "https://api-stagging.z1platform.com/api/s60/api/v2/"));
+
+    runApp(const MyApp());
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
+}
