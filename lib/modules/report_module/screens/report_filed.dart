2040384586b65dbfc0764ed75d6391162d6d0e30
SRENGHANY <hany.sreng@gmail.com>
2022-11-24 14:19:44 +0700

    updated_report

--- lib/modules/report_module/screens/report_filed.dart
+++ lib/modules/report_module/screens/report_filed.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 
 import '../../../Utils/helper/custom_appbar.dart';
 import '../../../Utils/option_controller/option_controller.dart';
+import '../../../widgets/bonus/custom_empty_state.dart';
 import '../../../widgets/privilege/privilege/custom_formfield_search.dart';
 import '../../../widgets/report/custom_list_with_title.dart';
 import '../../../widgets/report/report_shimmer.dart';
@@ -80,49 +81,54 @@ class _FileCategoriesReportState extends State<FileCategoriesReport> {
                     child: ReportShimmer(),
                   );
                 })
-            : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      height: 38,
-                      width: double.infinity,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 11,
-                      ),
-                      margin: const EdgeInsets.only(
-                          top: 20.0, right: 20.0, left: 20.0),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: CustomFormFieldSearch(
-                        onSaved: (e) {},
-                        onChanged: (v) {
-                          onChangeHandler(v);
-                        },
-                        controller: documentCon.textSearchController,
-                        keyboardType: TextInputType.name,
-                      ),
+            : documentCon.modelReportGroupByYear.isNotEmpty
+                ? SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 38,
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 11,
+                          ),
+                          margin: const EdgeInsets.only(
+                              top: 20.0, right: 20.0, left: 20.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: CustomFormFieldSearch(
+                            onSaved: (e) {},
+                            onChanged: (v) {
+                              onChangeHandler(v);
+                            },
+                            controller: documentCon.textSearchController,
+                            keyboardType: TextInputType.name,
+                          ),
+                        ),
+                        // Expanded(
+                        //   child: InvestorPage(
+                        //     tabLabel: docTypeCon.documentTypeList[widget.currentTabIndex].id
+                        //         .toString(),
+                        //   ),
+                        // ),
+                        Column(
+                          children: documentCon.modelReportGroupByYear
+                              .map(
+                                (e) => CustomListWithTitle(
+                                  year: e.date.toString(),
+                                  items: e.reports ?? [],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ],
                     ),
-                    // Expanded(
-                    //   child: InvestorPage(
-                    //     tabLabel: docTypeCon.documentTypeList[widget.currentTabIndex].id
-                    //         .toString(),
-                    //   ),
-                    // ),
-                    Column(
-                      children: documentCon.modelReportGroupByYear
-                          .map(
-                            (e) => CustomListWithTitle(
-                              year: e.date.toString(),
-                              items: e.reports ?? [],
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ],
-                ),
-              ),
+                  )
+                : const CustomEmptyState(
+                    title: 'Report',
+                    description: 'No File Report !',
+                  ),
       ),
     );
   }
