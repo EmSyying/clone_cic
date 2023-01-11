b5b258a05382475033cc5cfb9b755240537d0f2b
srenghany <hany.sreng@z1fsc.com>
2023-01-11 13:09:01 +0700

    updated

--- lib/modules/privilege_program/screen/privilege_detail/redeem_point_pay_screen.dart
+++ lib/modules/privilege_program/screen/privilege_detail/redeem_point_pay_screen.dart
@@ -80,8 +80,8 @@ class RedeemPointToPay extends StatelessWidget {
                                       decimal: true, signed: false),
                               isRequired: true,
                               autoFocus: false,
-                              labelText: 'Receiver Account Number',
-                              hintText: 'Receiver Account Number',
+                              labelText: 'Receiver Phone Number',
+                              hintText: 'Receiver Phone Number',
                               onChange: (value) {
                                 priController.receiveAccountNumber.value =
                                     value;
