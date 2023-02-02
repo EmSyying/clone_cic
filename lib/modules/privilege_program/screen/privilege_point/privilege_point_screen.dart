fe9a8bec6be31f0dcf1381b1a42f41c2d55b0c3f
YI NG <mac@macs-MacBook-Pro-4.local>
2023-02-02 13:55:41 +0700

    submit

--- lib/modules/privilege_program/screen/privilege_point/privilege_point_screen.dart
+++ lib/modules/privilege_program/screen/privilege_point/privilege_point_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 import '../../../../Utils/helper/firebase_analytics.dart';
@@ -79,58 +80,20 @@ class _PrivilegePointScreenState extends State<PrivilegePointScreen> {
             ),
 
             ///experiment
-            Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-              ),
-              body: Obx(
-                () => Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    height: 140,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'MVP Balance',
-                          style: textStyle.copyWith(
-                              fontWeight: FontWeight.w500, fontSize: 14),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          _walletController.mvpBalance.value.mvpAmountFormat ??
-                              '0.00',
-                          style: textStyle.copyWith(
-                              fontWeight: FontWeight.w700, fontSize: 30),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'As of ${FormatDate.investmentDateDisplayUTPrice(datetime)}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6!
-                              .copyWith(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 12,
-                                  color: const Color(0xfff2f2f2)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            // Scaffold(
+            //   backgroundColor: Colors.transparent,
+            //   appBar: AppBar(
+            //     backgroundColor: Colors.transparent,
+            //     elevation: 0,
+            //   ),
+            //   body: Obx(
+            //     () => Container(
+            //       width: double.infinity,
+            //       height: double.infinity,
+            //       alignment: Alignment.topLeft,
+            //       child:
+            //   ),
+            // ),
             Scaffold(
               backgroundColor: Colors.transparent,
               appBar: AppBar(
@@ -140,9 +103,110 @@ class _PrivilegePointScreenState extends State<PrivilegePointScreen> {
               ),
               body: NestedScrollView(
                 headerSliverBuilder: (context, _) => [
-                  SliverPersistentHeader(
-                    delegate: CustomSliver(height: 140),
-                  )
+                  SliverAppBar(
+                    backgroundColor: Colors.transparent,
+                    automaticallyImplyLeading: false,
+                    expandedHeight: 140,
+                    floating: true,
+                    pinned: false,
+                    flexibleSpace: FlexibleSpaceBar(
+                      collapseMode: CollapseMode.none,
+                      background: Container(
+                        height: 140,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    debugPrint('Worked');
+                                  },
+                                  child: Text(
+                                    'MVP Balance',
+                                    style: textStyle.copyWith(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  _walletController
+                                          .mvpBalance.value.mvpAmountFormat ??
+                                      '0.00',
+                                  style: textStyle.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 30),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'As of ${FormatDate.investmentDateDisplayUTPrice(datetime)}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline6!
+                                      .copyWith(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                          color: const Color(0xfff2f2f2)),
+                                ),
+                              ],
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                context.push('/wallet/point-exchange');
+                                // Navigator.push(
+                                //   context,
+                                //   MaterialPageRoute(
+                                //     builder: ((context) => WalletExchange()),
+                                //   ),
+                                // );
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 15),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.white.withOpacity(0.2)),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset(
+                                        'assets/images/svgfile/get_mvp.svg'),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'Get MVP',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline6!
+                                          .copyWith(
+                                              fontSize: 11,
+                                              fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  // SliverPersistentHeader(
+                  //   delegate: CustomSliver(
+                  //     height: 140,
+                  //     child:
+                  //   ),
+                  // )
                 ],
                 body: ConnectivityWidgetWrapper(
                   stacked: false,
@@ -245,7 +309,8 @@ class _PrivilegePointScreenState extends State<PrivilegePointScreen> {
 
 class CustomSliver extends SliverPersistentHeaderDelegate {
   final double? height;
-  CustomSliver({this.height});
+  final Widget? child;
+  CustomSliver({this.height, this.child});
 
   @override
   double get maxExtent => height ?? 0;
@@ -260,6 +325,6 @@ class CustomSliver extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return const SizedBox.shrink();
+    return child ?? const SizedBox.shrink();
   }
 }
