673cd3db159a769858759789083fe5517b769413
Touch-Theara <dev.theara@gmail.com>
2023-03-21 16:00:38 +0700

    update

--- lib/widgets/privilege/privilege_gift_mvp/custom_card_gift_mvp_form.dart
+++ lib/widgets/privilege/privilege_gift_mvp/custom_card_gift_mvp_form.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../../../Utils/helper/color.dart';
+import '../../../modules/privilege_program/screen/choose_gift_template.dart';
+import '../../custom_menu_holder.dart';
 
 class CustomCardGiftMVPForm extends StatelessWidget {
   final String? acountName;
@@ -24,6 +27,7 @@ class CustomCardGiftMVPForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final txtTheme = Theme.of(context).textTheme;
     return Container(
       height: 70.0,
       width: double.infinity,
@@ -94,17 +98,67 @@ class CustomCardGiftMVPForm extends StatelessWidget {
               ],
             ),
           ),
-          //More Option
 
-          PopupMenuButton(
-            color: Colors.white,
-            offset: const Offset(0, 35),
-            padding: EdgeInsets.zero,
-            elevation: 1,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            icon: const Align(
+          CustomFocusedMenuHolder(
+            openWithTap: true,
+            blurSize: 0,
+            menuWidth: (MediaQuery.of(context).size.width * 0.6),
+            blurBackgroundColor: Colors.grey[900]!.withOpacity(0.2),
+            menuItems: [
+              CustomFocusedMenuItem(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  paddingBetweenItem: const EdgeInsets.only(right: 13),
+                  title: Text(
+                    "Transaction History",
+                    style: txtTheme.bodyMedium!.copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  onPressed: () {
+                    onTapHistory?.call();
+                  },
+                  leadingIcon: SvgPicture.asset(
+                      "assets/images/transaction-history.svg")),
+              CustomFocusedMenuItem(
+                  paddingBetweenItem: const EdgeInsets.only(right: 13),
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  title: Text(
+                    "Edit Template",
+                    style: txtTheme.bodyMedium!.copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  onPressed: () {
+                    onTapEdit?.call();
+                  },
+                  leadingIcon:
+                      SvgPicture.asset("assets/images/edit-pencil.svg")),
+              CustomFocusedMenuItem(
+                  paddingBetweenItem: const EdgeInsets.only(right: 13),
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  title: Text(
+                    "Delete Template",
+                    style: txtTheme.bodyMedium!.copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  onPressed: () {
+                    // onTapDeleted?.call();
+                    if (Platform.isIOS) {
+                      buildAlertIos(context, txtTheme, id);
+                    } else {
+                      buildAlertAndroid(context, txtTheme, id);
+                    }
+                  },
+                  leadingIcon: SvgPicture.asset("assets/images/trash.svg"))
+            ],
+            onPressed: () {
+              debugPrint('Hello');
+            },
+            child: const Align(
               alignment: Alignment.topRight,
               child: Icon(
                 Icons.more_vert,
@@ -112,104 +166,125 @@ class CustomCardGiftMVPForm extends StatelessWidget {
                 color: Colors.black,
               ),
             ),
-            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-              PopupMenuItem(
-                padding: const EdgeInsets.only(right: 10, left: 10),
-                child: GestureDetector(
-                  onTap: onTapHistory,
-                  child: Container(
-                    color: Colors.transparent,
-                    height: 49,
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 18.0),
-                        SvgPicture.asset(
-                          'assets/images/history.svg',
-                          color: Colors.black,
-                          width: 22,
-                        ),
-                        const SizedBox(width: 15.0),
-                        Expanded(
-                          child: Text(
-                            "Transaction History",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleSmall!
-                                .copyWith(
-                                    fontWeight: FontWeight.w400,
-                                    color: AppColor.darkColor),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const PopupMenuDivider(height: 0),
-              PopupMenuItem(
-                padding: const EdgeInsets.only(right: 10, left: 10),
-                child: GestureDetector(
-                  onTap: onTapEdit,
-                  child: Container(
-                    height: 49,
-                    color: Colors.transparent,
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 18.0),
-                        SvgPicture.asset(
-                          'assets/images/menu_edit.svg',
-                        ),
-                        const SizedBox(width: 15.0),
-                        Expanded(
-                          child: Text(
-                            "Edit Template",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleSmall!
-                                .copyWith(
-                                    fontWeight: FontWeight.w400,
-                                    color: AppColor.darkColor),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const PopupMenuDivider(height: 0),
-              PopupMenuItem(
-                padding: const EdgeInsets.only(right: 10, left: 10),
-                child: GestureDetector(
-                  onTap: onTapDeleted,
-                  child: Container(
-                    color: Colors.transparent,
-                    height: 49,
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 18.0),
-                        SvgPicture.asset(
-                          'assets/images/new_getfunding_delete.svg',
-                          color: Colors.black,
-                        ),
-                        const SizedBox(width: 15.0),
-                        Expanded(
-                          child: Text(
-                            "Delete Template",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleSmall!
-                                .copyWith(
-                                    fontWeight: FontWeight.w400,
-                                    color: AppColor.darkColor),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+          )
+
+          //More Option
+
+          // PopupMenuButton(
+          //   color: Colors.white,
+          //   offset: const Offset(0, 35),
+          //   padding: EdgeInsets.zero,
+          //   elevation: 1,
+          //   shape: const RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          //   ),
+          //   icon: const
+          // Align(
+          //     alignment: Alignment.topRight,
+          //     child: Icon(
+          //       Icons.more_vert,
+          //       size: 20,
+          //       color: Colors.black,
+          //     ),
+          //   ),
+          //   itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+          //     PopupMenuItem(
+          //       padding: const EdgeInsets.only(right: 10, left: 10),
+          //       child: GestureDetector(
+          //         onTap: onTapHistory,
+          //         child: Container(
+          //           color: Colors.transparent,
+          //           height: 49,
+          //           child: Row(
+          //             children: [
+          //               const SizedBox(width: 18.0),
+          //               SvgPicture.asset(
+          //                 'assets/images/history.svg',
+          //                 color: Colors.black,
+          //                 width: 22,
+          //               ),
+          //               const SizedBox(width: 15.0),
+          //               Expanded(
+          //                 child: Text(
+          //                   "Transaction History",
+          //                   style: Theme.of(context)
+          //                       .textTheme
+          //                       .titleSmall!
+          //                       .copyWith(
+          //                           fontWeight: FontWeight.w400,
+          //                           color: AppColor.darkColor),
+          //                 ),
+          //               ),
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //     const PopupMenuDivider(height: 0),
+          //     PopupMenuItem(
+          //       padding: const EdgeInsets.only(right: 10, left: 10),
+          //       child: GestureDetector(
+          //         onTap: onTapEdit,
+          //         child: Container(
+          //           height: 49,
+          //           color: Colors.transparent,
+          //           child: Row(
+          //             children: [
+          //               const SizedBox(width: 18.0),
+          //               SvgPicture.asset(
+          //                 'assets/images/menu_edit.svg',
+          //               ),
+          //               const SizedBox(width: 15.0),
+          //               Expanded(
+          //                 child: Text(
+          //                   "Edit Template",
+          //                   style: Theme.of(context)
+          //                       .textTheme
+          //                       .titleSmall!
+          //                       .copyWith(
+          //                           fontWeight: FontWeight.w400,
+          //                           color: AppColor.darkColor),
+          //                 ),
+          //               )
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //     const PopupMenuDivider(height: 0),
+          //     PopupMenuItem(
+          //       padding: const EdgeInsets.only(right: 10, left: 10),
+          //       child: GestureDetector(
+          //         onTap: onTapDeleted,
+          //         child: Container(
+          //           color: Colors.transparent,
+          //           height: 49,
+          //           child: Row(
+          //             children: [
+          //               const SizedBox(width: 18.0),
+          //               SvgPicture.asset(
+          //                 'assets/images/new_getfunding_delete.svg',
+          //                 color: Colors.black,
+          //               ),
+          //               const SizedBox(width: 15.0),
+          //               Expanded(
+          //                 child: Text(
+          //                   "Delete Template",
+          //                   style: Theme.of(context)
+          //                       .textTheme
+          //                       .titleSmall!
+          //                       .copyWith(
+          //                           fontWeight: FontWeight.w400,
+          //                           color: AppColor.darkColor),
+          //                 ),
+          //               ),
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
