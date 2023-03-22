e7cc1f9c9a769f12f279f3c00f61ac5a8dde608c
kithya <kithya.um@z1fsc.com>
2023-03-22 15:59:57 +0700

    update mvp transaction template

--- lib/modules/privilege_program/screen/choose_gift_template.dart
+++ lib/modules/privilege_program/screen/choose_gift_template.dart
@@ -530,174 +530,212 @@ class ChooseGiftTemplateScreen extends StatelessWidget {
 
 Widget emtyStateTransactionTemplate(
     BuildContext context, ChosenMVPModel? chosenMVPModel) {
-  return Column(
-    children: [
-      Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 10),
-            width: 50,
-            height: 5,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                color: Colors.grey[300]),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          //Header Card Gift Template
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: headerCardGiftTemplate(
-              context,
-              titleGiftTemplate:
-                  chosenMVPModel != null ? chosenMVPModel.receiverName : "",
-              acountNumGiftTemplate:
-                  chosenMVPModel != null ? chosenMVPModel.receiverWallet : "",
-              id: chosenMVPModel != null ? chosenMVPModel.id : 0,
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 14.0, horizontal: 20.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Transaction History',
-                style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    color: const Color(0xff848F92)),
-              ),
-            ),
-          ),
-          Divider(
-            //thickness: 1.2,
-            color: Colors.grey[400],
-            height: 1,
-          ),
-        ],
-      ),
-      Expanded(
-        child: Container(
-          color: Colors.white,
-          width: double.infinity,
-          padding: const EdgeInsets.only(bottom: 30.0, top: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/emptyState.png',
-                // width: 180,
-                // height: 180,
-              ),
-              Text(
-                'No Transaction History',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium!
-                    .copyWith(fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 6.0,
-              ),
-              Text(
-                'No Transaction History Here !',
-                style: Theme.of(context)
-                    .textTheme
-                    .displayMedium!
-                    .copyWith(fontWeight: FontWeight.w400),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-      ),
-    ],
-  );
-}
-
-Widget loadingTransactionTemplate(
-    BuildContext context, ChosenMVPModel? chosenMVPModel) {
-  return Stack(
-    clipBehavior: Clip.hardEdge,
-    children: [
-      Column(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10))),
+  return LayoutBuilder(builder: (context, constrain) {
+    debugPrint("constrain:==${constrain.maxHeight}");
+    return Stack(
+      children: [
+        Container(
+          child: SingleChildScrollView(
+            physics: constrain.maxHeight > 600
+                ? const NeverScrollableScrollPhysics()
+                : const AlwaysScrollableScrollPhysics(),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  width: 50,
-                  height: 5,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.grey[300]),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                //Header Card Gift Template
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 10.0),
-                  child: headerCardGiftTemplate(
-                    context,
-                    titleGiftTemplate: chosenMVPModel != null
-                        ? chosenMVPModel.receiverName
-                        : "",
-                    acountNumGiftTemplate: chosenMVPModel != null
-                        ? chosenMVPModel.receiverWallet
-                        : "",
-                    id: chosenMVPModel != null ? chosenMVPModel.id : 0,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 14.0, horizontal: 20.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Transaction History',
-                      style: Theme.of(context)
-                          .textTheme
-                          .displayMedium!
-                          .copyWith(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: const Color(0xff848F92)),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 80,
                     ),
-                  ),
+                    //Header Card Gift Template
+
+                    Divider(
+                      //thickness: 1.2,
+                      color: Colors.grey[400],
+                      height: 1,
+                    ),
+                  ],
                 ),
-                Divider(
-                  //thickness: 1.2,
-                  color: Colors.grey[400],
-                  height: 1,
+                Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(bottom: 30.0, top: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/emptyState.png',
+                        // width: 180,
+                        // height: 180,
+                      ),
+                      Text(
+                        'No Transaction History',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 6.0,
+                      ),
+                      Text(
+                        'No Transaction History Here !',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(fontWeight: FontWeight.w400),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(top: Get.height * .15),
-            child: Center(
-              child: CircularProgressIndicator(
-                color: Theme.of(context).primaryColor,
+        ),
+        Container(
+          // color: Colors.white,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+              color: Colors.white),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                width: 50,
+                height: 5,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.grey[300]),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              //Header Card Gift Template
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 10.0),
+                child: headerCardGiftTemplate(
+                  context,
+                  titleGiftTemplate:
+                      chosenMVPModel != null ? chosenMVPModel.receiverName : "",
+                  acountNumGiftTemplate: chosenMVPModel != null
+                      ? chosenMVPModel.receiverWallet
+                      : "",
+                  id: chosenMVPModel != null ? chosenMVPModel.id : 0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 14.0, horizontal: 20.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Transaction History',
+                    style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xff848F92)),
+                  ),
+                ),
+              ),
+              Divider(
+                //thickness: 1.2,
+                color: Colors.grey[400],
+                height: 1,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  });
+}
+
+Widget loadingTransactionTemplate(
+    BuildContext context, ChosenMVPModel? chosenMVPModel) {
+  return LayoutBuilder(builder: (context, constrain) {
+    debugPrint("constrain:==${constrain.maxHeight}");
+    return Stack(
+      clipBehavior: Clip.hardEdge,
+      children: [
+        Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10))),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    width: 50,
+                    height: 5,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.grey[300]),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  //Header Card Gift Template
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 10.0),
+                    child: headerCardGiftTemplate(
+                      context,
+                      titleGiftTemplate: chosenMVPModel != null
+                          ? chosenMVPModel.receiverName
+                          : "",
+                      acountNumGiftTemplate: chosenMVPModel != null
+                          ? chosenMVPModel.receiverWallet
+                          : "",
+                      id: chosenMVPModel != null ? chosenMVPModel.id : 0,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 14.0, horizontal: 20.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Transaction History',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: const Color(0xff848F92)),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    //thickness: 1.2,
+                    color: Colors.grey[400],
+                    height: 1,
+                  ),
+                ],
               ),
             ),
-          )
-        ],
-      ),
-    ],
-  );
+            Padding(
+              padding: EdgeInsets.only(top: Get.height * .15),
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  });
 }
 
 void buildAlertIos(context, TextTheme txtTheme, int? deleteID) {
